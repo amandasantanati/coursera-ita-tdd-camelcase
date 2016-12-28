3c0db9411db68a9f8974655038030a003b4845d8
Amanda Santana <asantana@thoughtworks.com>
2016-12-28 10:59:27 -0300

    Refatora para melhor regidibilidade

--- src/StringConverter.java
+++ src/StringConverter.java
@@ -1,26 +1,32 @@
 import java.util.ArrayList;
-import java.util.IllegalFormatException;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringConverter {
     private static final String REPLACEMENT = " ";
+    private static final String MESSAGE_INVALID_NUMBER_START = "Não pode começar com número";
+    private static final String MSG_INVALID_CARACTERES = "Não pode conter caracteres inválidos";
+    private static final String REGEX_INVALID_CARACTERES = "[~#@*+%{}<>\\[\\]|\"\\_^]";
+    private static final String UPPERCASE_BEFORE_LOWER_OR_UPPERCASE = "(?<=[A-Z])(?=[A-Z][a-z])";
+    private static final String LOWERCASE_BEFORE_UPPERCASE = "(?<=[a-z])(?=[A-Z])";
+    private static final String UPPER_AND_LOWERCASE_BEFORE_DIGITS = "(?<=[A-Za-z])(?=[0-9])";
+    private static final String DIGITS_BEFORE_LOWER_AND_UPPERCASE = "(?<=[0-9])(?=[A-Za-z])";
 
     public static List<String> converterCamelCase(String original) {
         if(isBeginWithNUmber(original)) {
-            throw new IllegalArgumentException("Não pode começar com número");
+            throw new IllegalArgumentException(MESSAGE_INVALID_NUMBER_START);
         }
 
         if(containsIllegals(original)) {
-            throw new IllegalArgumentException("Não pode conter caracteres inválidos");
+            throw new IllegalArgumentException(MSG_INVALID_CARACTERES);
         }
 
         return splitWords(original);
     }
 
     private static boolean containsIllegals(String original) {
-        Pattern pattern = Pattern.compile("[~#@*+%{}<>\\[\\]|\"\\_^]");
+        Pattern pattern = Pattern.compile(REGEX_INVALID_CARACTERES);
         Matcher matcher = pattern.matcher(original);
         return matcher.find();
     }
@@ -32,10 +38,10 @@ public class StringConverter {
     private static String splitCamelCase(String original) {
         return original.replaceAll(
                 String.format("%s|%s|%s|%s",
-                        "(?<=[A-Z])(?=[A-Z][a-z])",
-                        "(?<=[a-z])(?=[A-Z])",
-                        "(?<=[A-Za-z])(?=[0-9])",
-                        "(?<=[0-9])(?=[A-Za-z])"
+                        UPPERCASE_BEFORE_LOWER_OR_UPPERCASE,
+                        LOWERCASE_BEFORE_UPPERCASE,
+                        UPPER_AND_LOWERCASE_BEFORE_DIGITS,
+                        DIGITS_BEFORE_LOWER_AND_UPPERCASE
                 ),
                 REPLACEMENT
         );
